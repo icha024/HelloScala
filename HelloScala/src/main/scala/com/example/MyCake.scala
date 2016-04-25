87c74fd35b6b32e2e2d4e5f05d347c65738003ac
Ian Chan <icha024@gmail.com>
2016-04-25 16:14:08 +0100

    Polishing

--- HelloScala/src/main/scala/com/example/MyCake.scala
+++ HelloScala/src/main/scala/com/example/MyCake.scala
@@ -1,30 +1,29 @@
 package com.example
 
 object MyCake extends App {
-  trait GenericDao {
+
+  trait GenericDao { // This is an 'interface'
     def Imp: String
   }
 
-  trait SqlDao extends GenericDao {
+  trait SqlDao extends GenericDao { // This is like implementing interface with default in Java8 (An implementation)
     def Imp: String = "SQL Implementation"
   }
 
-  trait CloudantDao extends GenericDao {
+  trait CloudantDao extends GenericDao { // This is like implementing interface with default in Java8 (An implementation)
     def Imp: String = "Cloudant Implementation"
   }
 
   trait MyServiceTrait {
-    dao: GenericDao =>
-    //println("My implementation is: " + dao.Imp)
-  }
-
-  class FunService {
+    // This relies on 'interface' instead of 'implementation' - it just normal Java/Spring best practice anyway.
+    dao: GenericDao => // Syntax for trait 'self type annotation', we really should just call it 'extending an interface'
+    println("My implementation is: " + dao.Imp)
   }
 
   val svc = new MyServiceTrait with CloudantDao
-  println("Using cloud DI: " + svc.Imp)
+  println("svc using Cloudant DI returns: " + svc.Imp)
 
   val svc2 = new MyServiceTrait with SqlDao
-  println("Using SQL DI: " + svc2.Imp)
+  println("svc2 using SQL DI returns: " + svc2.Imp)
 
 }
--- HelloScala/src/main/scala/com/example/MyMagnetChecker.scala
+++ HelloScala/src/main/scala/com/example/MyMagnetChecker.scala
@@ -22,9 +22,12 @@ object MyMagnetChecker extends App {
   }
 
   def findStickyType(mag: MyMagnet): Unit = {
-    println("finding magnet of some type.... " + mag.getRes)
+    println("Finding magnet of some type... " + mag.getRes)
   }
 
   findStickyType("abc"); // create 'MyMagnet' with String constructor
   findStickyType(123);   // create 'MyMagnet' with Int constructor
+
+  // Magnet pattern = Instead of method overloading, we use one big pojo with variable amount of fields.
+  // Then instead of messy if/else checks on each field of the big pojo to run different init, we use 'implicit' method feature of Scala.
 }
